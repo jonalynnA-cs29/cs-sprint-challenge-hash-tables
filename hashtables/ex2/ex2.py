43c8dbc6996f0034b49aa9212864df7aed088b53
jonalynn masters <jonalynna.masters@gmail.com>
2020-06-08 18:20:19 -0700

    ex2 completed test passes

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,24 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # set up route list with slots
+    route = [None] * length
 
+    route_find = {}
+
+    # store the ticket
+    for ticket in tickets:
+        route_find[ticket.source] = ticket.destination
+
+    # Start with None
+    next_route = route_find["NONE"]
+
+    # look up each next route and stop when next route is NONE
+    for current in range(0, length):
+
+        # store next route
+        route[current] = next_route
+
+        # recall next route
+        next_route = route_find[next_route]
     return route
